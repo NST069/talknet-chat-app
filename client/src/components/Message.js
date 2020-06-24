5b6bdc81ca6af4d3984bba121d0f80a537800b51
Nikolay Dvachevsky <nislav.ns@gmail.com>
2020-06-24 15:38:01 +0300

    Sending messages to special user

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -5,16 +5,22 @@ const Message = ({message: {user, text}, name})=>{
     const trimmedName= name.trim().toLowerCase();
 
     let sentByCurrentUser = user===trimmedName;
+    let sentToCurrentUser = text.includes(`@${name}`) || text.includes(`@${trimmedName}`);
     
     return(
         sentByCurrentUser
-        ?<li className="list-group-item active">
-            <p class="mb-1">{Reactemoji.emojify(text)}</p>
+        ?<li className="list-group-item list-group-item-primary">
+            <p className="mb-1">{Reactemoji.emojify(text)}</p>
         </li>
-        :<li class="list-group-item">
-            <p class="mb-1">{Reactemoji.emojify(text)}</p>
-            <small class="text-muted">{user}</small>
-        </li>
+        : sentToCurrentUser
+            ?<li className="list-group-item list-group-item-warning">
+                <p className="mb-1">{Reactemoji.emojify(text)}</p>
+                <small className="text-muted">{user}</small>
+            </li>
+            :<li className="list-group-item">
+                <p className="mb-1">{Reactemoji.emojify(text)}</p>
+                <small className="text-muted">{user}</small>
+            </li>
     );
 
 };
